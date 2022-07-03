0e85e92e1a3d1c871132e0ccfbebbd4349ac3983
Andres Acuna <andres.acu1997@gmail.com>
2022-07-03 17:29:17 -0500

    Product fetched by mock file

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getMock();
+  }, []);
+
+  const getMock = async () => {
+    try {
+      setLoading(true);
+      const req = await fetch("data/products.json");
+      const { data: dataReq } = await req.json();
+      await setProducts([...dataReq.products.items]);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>Products</div>
-  )
-}
+    <>
+      {loading ? (
+        <h3>Loading</h3>
+      ) : (
+        <>
+          <ul>
+            {products.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+};
